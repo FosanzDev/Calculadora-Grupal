c4b3d46d1667185cadb9dd7dcd2fa432d09e7620
Marco <sabariano@gmail.com>
2022-11-15 17:09:05 +0100

    Added arithmetic methods
    Se han añadido las operaciones aritmeticas de la calculadora grupal

--- lib/Arithmetics.java
+++ lib/Arithmetics.java
@@ -2,5 +2,44 @@ package lib;
 
 public class Arithmetics{
 
+    public static double sum(double num1, double num2){
+        double res=num1+num2;
+        return res;
+    }
+
+    public static int sum(int num1, int num2){
+        sin res=num1+num2;
+        return res;
+    }
     
+    public static double rest(double num1, double num2){
+        double res=num1-num2;
+        return res;
+    }
+
+    public static int rest(int num1, int num2){
+        sin res=num1-num2;
+        return res;
+    }
+
+    public static double mult(double num1, double num2){
+        double res=num1*num2;
+        return res;
+    }
+
+    public static int mult(int num1, int num2){
+        sin res=num1*num2;
+        return res;
+    }
+
+    public static double div(double num1, double num2){
+        double res=num1/num2;
+        return res;
+    }
+
+      public static double rest(double num1, double num2){
+        double res=num1%num2;
+        return res;
+    }
+
 }
