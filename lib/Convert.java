1d2fb68b69c993bd1212d5c38aec6481117f9c2a
Jvengut <115951062+Jvengut@users.noreply.github.com>
2022-11-16 12:52:43 +0100

    Añadido primer metodo
    Se ha añadido el primero metodo que es bin2dec

--- lib/Convert.java
+++ lib/Convert.java
@@ -1,5 +1,22 @@
 package lib;
 
 public class Convert{
+
+    public static int bin2dec(String binaryNumber) {
+        int res = 0;
+        int idx = 0;
+
+        for (int i = binaryNumber.length()-1; i >= 0 ; i --) {
+            char c = binaryNumber.charAt(i);
+
+            if (c == '1') {
+                int num = Character.getNumericValue(c);
+                double operation = num * Math.pow(2, idx);
+                res += operation;
+            }
+            idx++;
+        }
+        return res;
+    }
     
 }
