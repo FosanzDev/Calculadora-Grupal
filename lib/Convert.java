8e3a9e428deb4974d9b921758f31adb053c9101c
Jvengut <115951062+Jvengut@users.noreply.github.com>
2022-11-16 13:04:04 +0100

    Añadidos los javadocs de los metodos
    He añadidos las descripciones y ejemplos de los metodos

--- lib/Convert.java
+++ lib/Convert.java
@@ -2,6 +2,19 @@ package lib;
 
 public class Convert{
 
+    /**
+     * Este método realizará una conversion de un numero binario a un numero decimal el cual introducirá el usuario.
+     * <p>
+     * Ejemplo:
+     * <blockquote><pre>
+     * long result = bin2dec("00010011")
+     * // result = 19
+     * 
+     * @param res creamos una variable para almacenar el resultado
+     * @param idx creamos una variable para poder guardar los indices
+     * @param binaryNumber para indicar que el numero es binario
+     * @return 
+    */
     public static int bin2dec(String binaryNumber) {
         int res = 0;
         int idx = 0;
@@ -19,6 +32,21 @@ public class Convert{
         return res;
     }
     
+    /**
+     * Este metodo sirve para poder realizar una conversion de un numoer decimal a un numero hexadecimal
+     * <p>
+     * Ejemplo:
+     * * <blockquote><pre>
+     * long result = dec2hex("43")
+     * // result = 2B
+     * 
+     * 
+     * @param numero variable para almacenar el numero que nos pase el usuario
+     * @param resto variable para almacenar el el resto del numero
+     * @param String variable que almacena el resultado 
+     * @return retorna el resultado final con la conversion de decimal a hexadecimal hecha
+     */
+
     public static String dec2hex(int numero){
 		int resto;
        	String resultado="";
@@ -31,6 +59,18 @@ public class Convert{
  		}
 		return resultado;
 	}
+
+    /**
+     * Este metodo aprovecha los dos anteriores para hacer uno nuevo que realize conversiones de binario a hexadecimal
+     * <p>
+     * Ejemplo:
+     *  <blockquote><pre>
+     * long result = bin2hex("10100101")
+     * // result = A5
+     * 
+     * @param binaryNumber para indicar que estamos utilizando un nunmero binario
+     * @return lo que retorna es el resultado final que da utilizando los dos metodos para realizar las conversiones necesarias
+    */
 
 
     public static String bin2hex(String binaryNumber){
