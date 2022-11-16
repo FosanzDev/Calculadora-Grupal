edc04a5bae9e75ae2395018f25340d2bd3b08a35
adriaabf04 <adrianbf2004@gmail.com>
2022-11-16 12:45:38 +0100

    Correct some errors and added javadoc comments

--- lib/Convert.java
+++ lib/Convert.java
@@ -1,60 +1,68 @@
 package lib;
+
 public class Convert {
     /**
-     * In this function will return the {@code binary}number and request a {@code String}
+     * In this method, we will pass from hex to dec. We request a hexadecimal number to pass it from decimal number
      * <p>
-     * Example: 
-     * <blockquote><pre>
-     * bin2dec("001011")
-     * res = binaryNumber.charAt(i) * Math.pow(2, i)
-     * //i is the times that the code travel until i arrives at the length of the String
-     * //2 is the number that is multiplied 
-     * </pre></blockquote>
-     * <p>
-     * @param binaryNumber (String that is a binary number)
-     * @return res (The decimal number)
-     */
-    public static int bin2dec(String binaryNumber) {
-        int res = 0;
-        int resOperation = 0;
-        for (int i = 0; i < binaryNumber.length(); i ++) {
-            char c = binaryNumber.charAt(i);
-            if (c == '1') {
-                int num = Character.getNumericValue(c);
-                double operation = num * Math.pow(2, i);
-                resOperation = (int) operation;
-                res += operation;
-            }
-        }
-        return res;
-    }
-    /**
-     * In this function, it will return a {@code decimal} that before was an {@code hexadecimal} number
-     * <p>
-     * Example: 
      * <blockquote><pre>
      * hex2dec("3F5");
-     * //outputs 3155 because : 3 ---> 3 | F ---> 15 | 5 ---> 5
-     * //And all of this numbers go to String res, that then, will be output
+     * //outputs 1013 
      * </blockquote></pre>
-     * @param hexNumber (The character in hexadecimal)
-     * @return res (That is the result of the decimal number)
+     * <p>
+     * @param hex The hexadecimal number
+     * @return The decimal number
      */
-    public static String hex2dec(String hexNumber) {
+    public static int hex2dec(String hex) {
         String hexConstants = "ABCDEF";
-        String res = "";
+        int res = 0;
         int decNum;
+        int indx = 0;
+        hex = hex.toUpperCase().trim();
 
-        for (int i = 0; i < hexNumber.length(); i ++) {
-            decNum = 0;
-            char c = hexNumber.charAt(i);
+        for (int i = hex.length()-1; i >= 0; i--) {
+            char c = hex.charAt(i);
             if (c > '9') {
                 decNum = 10 + hexConstants.indexOf(c);
             } else {
                 decNum = c - '0';
-            }
-            res += decNum;
+            }  
+
+            double operation = decNum * Math.pow(16,indx);
+            res += operation;
+            indx ++;
         }
         return res;
+    }
+    /**
+     * In this method, we will pass from dec to bin. We request a decimal number to pass it from binary number
+     * <blockquote><pre>
+     * dec2bin(33);
+     * //outputs 0010 0001
+     * </blockquote></pre>
+     * <p>
+     * @param dec The decimal number
+     * @return The binary number
+     */
+    public static String dec2bin(int dec) {
+        int resto;
+        StringBuilder binary = new StringBuilder();
+        do {
+            resto = dec % 2;
+            dec /= 2;
+            binary.append(resto);
+        } while (dec != 0);
+        return binary.reverse().toString();
+    }
+    /**
+     * In this method, we will pass from hex to bin. We request a hexadecimal number to pass it from binary number, first passing it from hexadecimal to decimal and after decimal to binary number
+     * <blockquote><pre>
+     * hex2bin("3F5");
+     * //outputs 0010 0001
+     * </blockquote></pre>
+     * @param hex (The hexadecimal number)
+     * @return The binary number
+     */
+    public static String hex2bin(String hex) {
+        return dec2bin(hex2dec(hex));
     }
 }
